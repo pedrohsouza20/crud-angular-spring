c4981ad31fb7f0e95b540eaee5d958877c9d02ef
Pedro Souza <contato.pedromoc@gmail.com>
2024-02-22 23:10:32 -0300

    added Lazy-Loading on course's module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'courses'
+  },
+
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then((m: any) => m.CoursesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
