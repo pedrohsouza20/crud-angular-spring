df5da844f3c1ec584dc04f77b47cb27d37080596
Pedro Souza <contato.pedromoc@gmail.com>
2024-02-28 01:43:03 -0300

    Created service of courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,24 +1,18 @@
 import { Component } from '@angular/core';
 
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
-
 export class CoursesComponent {
-  courses: Course[] = [
-    {
-      _id: "1",
-      name: "Angular",
-      category: "Front-end"
-    }
-  ];
+  courses: Course[];
   displayedColumns = ['name', 'category'];
 
-  constructor(){
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
   }
-
 }
