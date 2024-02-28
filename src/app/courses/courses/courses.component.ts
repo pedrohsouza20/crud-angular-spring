fdf915eb5237e7c94dea05567c57521d49a6fbc4
Pedro Souza <contato.pedromoc@gmail.com>
2024-02-27 21:43:20 -0300

    Added Angular Material's table on courses module

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
+
 export class CoursesComponent {
+  courses: Course[] = [
+    {
+      _id: "1",
+      name: "Angular",
+      category: "Front-end"
+    }
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor(){
+  }
 
 }
